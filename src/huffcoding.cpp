08505d153b37757c94e29bd8f63e3c12f3837e11
tomislaaaav <m_botelho8@hotmail.com>
2015-05-22 01:33:26 +0100

    vem que o amor não é o tempo

--- src/huffcoding.cpp
+++ src/huffcoding.cpp
@@ -25,100 +25,7 @@ extern char* optarg;
 static int memory_encode_file(FILE *in, FILE *out);
 static int memory_decode_file(FILE *in, FILE *out);
 
-static void version(FILE *out)
-{
-	fputs("huffcode 0.3\n"
-	      "Copyright (C) 2003 Douglas Ryan Richardson"
-	      "; Gauss Interprise, Inc\n",
-	      out);
-}
-
-static void usage(FILE* out)
-{
-	fputs("Usage: huffcode [-i<input file>] [-o<output file>] [-d|-c]\n"
-		  "-i - input file (default is standard input)\n"
-		  "-o - output file (default is standard output)\n"
-		  "-d - decompress\n"
-		  "-c - compress (default)\n"
-		  "-m - read file into memory, compress, then write to file (not default)\n",
-		  out);
-}
-/*
-int main(int argc, char** argv)
-{
-	char memory = 0;
-	char compress = 1;
-	int opt;
-	const char *file_in = NULL, *file_out = NULL;
-	FILE *in = stdin;
-	FILE *out = stdout;
-
-	// Get the command line arguments.
-	while((opt = getopt(argc, argv, "i:o:cdhvm")) != -1)
-	{
-		switch(opt)
-		{
-		case 'i':
-			file_in = optarg;
-			break;
-		case 'o':
-			file_out = optarg;
-			break;
-		case 'c':
-			compress = 1;
-			break;
-		case 'd':
-			compress = 0;
-			break;
-		case 'h':
-			usage(stdout);
-			return 0;
-		case 'v':
-			version(stdout);
-			return 0;
-		case 'm':
-			memory = 1;
-			break;
-		default:
-			usage(stderr);
-			return 1;
-		}
-	}
-
-	// If an input file is given then open it.
-	if(file_in)
-	{
-		in = fopen(file_in, "rb");
-		if(!in)
-		{
-			fprintf(stderr, "Can't open input file '%s': %s\n", file_in, strerror(errno));
-			return 1;
-		}
-	}
-
-	// If an output file is given then create it.
-	if(file_out)
-	{
-		out = fopen(file_out, "wb");
-		if(!out)
-		{
-			fprintf(stderr, "Can't open output file '%s': %s\n", file_out, strerror(errno));
-			return 1;
-		}
-	}
-
-	if(memory)
-	{
-		return compress ?
-			memory_encode_file(in, out) : memory_decode_file(in, out);
-	}
-
-	return compress ?
-		huffman_encode_file(in, out) : huffman_decode_file(in, out);
-}
-*/
-static int
-memory_encode_file(FILE *in, FILE *out)
+static int memory_encode_file(FILE *in, FILE *out)
 {
 	unsigned char *buf = NULL, *bufout = NULL;
 	unsigned int len = 0, cur = 0, inc = 1024, bufoutlen = 0;
@@ -166,8 +73,7 @@ memory_encode_file(FILE *in, FILE *out)
 	return 0;
 }
 
-static int
-memory_decode_file(FILE *in, FILE *out)
+static int memory_decode_file(FILE *in, FILE *out)
 {
 	unsigned char *buf = NULL, *bufout = NULL;
 	unsigned int len = 0, cur = 0, inc = 1024, bufoutlen = 0;
--- src/main.cpp
+++ src/main.cpp
@@ -13,98 +13,159 @@
 #include <iterator>
 #include <bitset>
 #include <sstream>
+#include <ctime>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-
 	readDir directories;
-	directories.readDirectories("C:\\Users\\Miguel\\Desktop");
-
-	/* COMPRESS AND DECOMPRESS WITH LZW*/
-	for (unsigned int i = 0; i < directories.getFilenames().size(); i++)
+	string arg2(argv[2]);
+	clock_t start_d;
+	start_d = clock();
+	if (directories.readDirectories(argv[2]) != 0)
 	{
-		vector<int> compressed;
+		return 1;
+	}
+	clock_t end_d;
+	end_d = clock();
+	cout << "Time elapsed reading the directory: " << (double)(end_d-start_d)/CLOCKS_PER_SEC << endl;
+	clock_t start;
+	start = clock();
 
-		string diretorioUtilizado;
-		diretorioUtilizado = "C:\\Users\\Miguel\\Desktop";
-		diretorioUtilizado.append("\\");
-		diretorioUtilizado.append(directories.getFilenames().at(i).c_str());
-
-		string diretorioComprimido;
-		diretorioComprimido = "lzw\\compressed_lzw_";
-		diretorioComprimido.append(directories.getFilenames().at(i).c_str());
-
-		ifstream temp;
-		temp.open(diretorioUtilizado.c_str(), ios::binary);
-
-		ofstream file;
-		file.open(diretorioComprimido.c_str(), ios::binary);
-
-		string conteudo;
-
-		while (!(temp.eof()))
+	if (strcmp(argv[1],"-lzw") == 0)
+	{
+		/* COMPRESS AND DECOMPRESS WITH LZW*/
+		for (unsigned int i = 0; i < directories.getFilenames().size(); i++)
 		{
-			string temp1;
-			getline(temp, temp1);
-			conteudo.append(temp1);
+			vector<int> compressed;
+
+			string diretorioUtilizado;
+			diretorioUtilizado = arg2;
+			diretorioUtilizado.append("\\");
+			diretorioUtilizado.append(directories.getFilenames().at(i).c_str());
+
+			string diretorioComprimido;
+			diretorioComprimido = "lzw\\compressed_lzw_";
+			diretorioComprimido.append(directories.getFilenames().at(i).c_str());
+
+			ifstream temp;
+			temp.open(diretorioUtilizado.c_str(), ios::binary);
+			if (!(temp.is_open()))
+			{
+				cout << "Erro a abrir o diretório introduzido pelo utilizador" << endl;
+				return 2;
+			}
+
+			ofstream file;
+			file.open(diretorioComprimido.c_str(), ios::binary);
+			if (!(file.is_open()))
+			{
+				cout << "Erro a abrir o diretório onde irão ser gravados os ficheiros" << endl;
+				return 3;
+			}
+
+			string conteudo;
+
+			while (!(temp.eof()))
+			{
+				string temp1;
+				getline(temp, temp1);
+				conteudo.append(temp1);
+			}
+
+			compress(conteudo, back_inserter(compressed));
+
+			directories.addCompressed(compressed);
+
+			copy(compressed.begin(), compressed.end(), ostream_iterator<int>(file));
+
+			temp.close();
+			file.close();
+
+			string decompressed = decompress(compressed.begin(), compressed.end());
+
+			string decompressed_name = "lzw\\decompressed_lzw_";
+			decompressed_name.append(directories.getFilenames().at(i).c_str());
+
+			ofstream decompressed_file;
+			decompressed_file.open(decompressed_name.c_str());
+			if (!(decompressed_file.is_open()))
+			{
+				cout << "Erro a abrir o diretório onde irão ser gravados os ficheiros" << endl;
+				return 4;
+			}
+
+			for (unsigned int j = 0; j < decompressed.length(); j++)
+			{
+				decompressed_file << decompressed.at(j);
+			}
+
+			decompressed_file.close();
 		}
 
-		compress(conteudo, back_inserter(compressed));
-
-		directories.addCompressed(compressed);
-
-		copy(compressed.begin(), compressed.end(), ostream_iterator<int>(file));
-
-		temp.close();
-		file.close();
-
-		string decompressed = decompress(compressed.begin(), compressed.end());
-
-		string decompressed_name = "lzw\\decompressed_lzw_";
-		decompressed_name.append(directories.getFilenames().at(i).c_str());
-
-		ofstream decompressed_file;
-		decompressed_file.open(decompressed_name.c_str());
-
-		for (unsigned int j = 0; j < decompressed.length(); j++)
-		{
-			decompressed_file << decompressed.at(j);
-		}
-
-		decompressed_file.close();
+		clock_t end;
+		end = clock();
+		cout << "Time elapsed doing lzw: " << (double)(end-start)/CLOCKS_PER_SEC;
 	}
 
-	/* COMPRESSED AND DECOMPRESSED WITH HUFFMAN */
-	for (unsigned int i = 0; i < directories.getFilenames().size(); i++)
+	else if (strcmp(argv[1],"-huff")==0)
 	{
-		string encode = "C:\\Users\\Miguel\\Desktop\\";
-		encode.append(directories.getFilenames().at(i));
+		clock_t start;
+		start = clock();
+		/* COMPRESSED AND DECOMPRESSED WITH HUFFMAN */
+		for (unsigned int i = 0; i < directories.getFilenames().size(); i++)
+		{
+			string encode = arg2;
+			encode.append("\\");
+			encode.append(directories.getFilenames().at(i));
 
-		string decode = "huff\\compressed_huff_";
-		decode.append(directories.getFilenames().at(i));
+			string decode = "huff\\compressed_huff_";
+			decode.append(directories.getFilenames().at(i));
 
-		FILE * in = fopen(encode.c_str(), "rb");
-		FILE * out = fopen(decode.c_str(), "wb");
+			FILE * in = fopen(encode.c_str(), "rb");
+			if (in == NULL)
+			{
+				cout << "Erro a abrir os ficheiros do diretório introduzidos pelo utilizador" << endl;
+				return 5;
+			}
+			FILE * out = fopen(decode.c_str(), "wb");
+			if (out == NULL)
+			{
+				cout << "Erro a abrir os ficheiros que irão ser gravados" << endl;
+				return 6;
+			}
 
-		memory_encode_file(in, out);
+			memory_encode_file(in, out);
 
-		string dec = "huff\\decompressed_huff_";
-		dec.append(directories.getFilenames().at(i));
+			string dec = "huff\\decompressed_huff_";
+			dec.append(directories.getFilenames().at(i));
 
-		FILE * in1 = fopen(decode.c_str(), "rb");
-		FILE * out1 = fopen(dec.c_str(), "wb");
+			FILE * in1 = fopen(decode.c_str(), "rb");
+			if (in1 == NULL)
+			{
+				cout << "Erro a abrir os ficheiros que irão ser gravados" << endl;
+				return 7;
+			}
+			FILE * out1 = fopen(dec.c_str(), "wb");
+			if (out1 == NULL)
+			{
+				cout << "Erro a abrir os ficheiros que irão ser gravados" << endl;
+				return 8;
+			}
 
-		memory_decode_file(in1, out1);
+			memory_decode_file(in1, out1);
 
-		fclose(in1);
-		fclose(in);
-		fclose(out);
-		fclose(out1);
+			fclose(in1);
+			fclose(in);
+			fclose(out);
+			fclose(out1);
 
+			clock_t end;
+			end = clock();
+			cout << "Time elapsed doing hzw: " << (double)(end-start)/CLOCKS_PER_SEC << endl;
+		}
 	}
-
 	return 0;
 }
 
